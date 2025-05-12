c2084c50ed436b4bf69ee1a9d37f06218cb0bfb7
3-126-7-PC <you@example.com>
2025-05-12 13:00:14 +1200

    12.05.25

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // #region Константы
-const host = 'ksdlakdlsa'
+const host = 'http://api-messenger.web-srv.local'
 const context = select('.content')
 // #endregion
 
 // #region Переменные
-let token = ''
+let bearer_token = ''
 let user_email = ''
 // #endregion
 
@@ -27,6 +27,31 @@ function load (url, c, callback) {
         }
     }
 }
+
+function post(params, callback) {
+    let xhr = new XMLHttpRequest ();
+    xhr.open('POST',params.url)
+    xhr.send(params.data)
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState==4){
+            callback(xhr.responseText)
+        }
+    }
+}
+
+function logout(params, callback) {
+    let xhr = new XMLHttpRequest ();
+    xhr.open('DELETE',params.url)
+    xhr.send()
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState==4){
+            callback(xhr.responseText)
+        }
+    }
+}
+
 // #endregion
 
 load('/modules/chats.html', context, onLoadChats)
@@ -36,8 +61,19 @@ function onLoadAuth() {
         load('/modules/registration.html', context, onLoadReg)
     })
     select('.login').addEventListener('click', function () {
-        load('/modules/chats.html', context, onLoadChats)
-        select('.callback').innerHTML = ''
+        let authData = new FormData()
+        authData.append('email', select('input[name="email"]').value)
+        authData.append('pass', select('input[name="pass"]').value)
+
+        post({url: `${host}/auth/`, data: authData}, function(response){
+            response = JSON.parse(response)
+            console.log(response)
+
+            if (response.message == "Доступ разрешен") {
+                bearer_token = response.token
+                load('/modules/chats.html', context, onLoadChats)
+            }
+        })
     })
 }
 
@@ -45,14 +81,45 @@ function onLoadReg(){
     select('.go-auth').addEventListener('click', function(){
         load('/modules/authorization.html', context, onLoadAuth)
     })
-        select('.login').addEventListener('click', function () {
-        load('/modules/chats.html', context, onLoadChats)
+        select('.register').addEventListener('click', function () {
+            let regData = new FormData();
+            regData.append('email', select('input[name="email"]').value)
+            regData.append('pass', select('input[name="pass"]').value)
+            regData.append('fam', select('input[name="last-name"]').value)
+            regData.append('name', select('input[name="first-name"]').value)
+            regData.append('otch', select('input[name="fam-name"]').value)
+
+            post({url: `${host}/user/`, data:regData}, function(response){
+                response = JSON.parse(response)
+                console.log(response)
+
+                if(response.message == "Регистрация успешна"){
+                    bearer_token = response.token
+                    
+                }
+                else{
+                    select('.callback').innerHTML = response.message
+                }
+            })
     })
 }
 
 function onLoadChats() {
     select('.logout').addEventListener('click', function(){
-        load('/modules/authorization.html', context, onLoadAuth)
-        select('.callback').innerHTML = 'Выход из приложения...'
+        logout({url: `${host}/auth/`}, function(response){
+            response = JSON.parse(response)
+            console.log(response)
+            bearer_token = ''
+
+            load('/modules/authorization.html', context, onLoadAuth)
+            select('.callback').innerHTML = 'Выход из приложения...'
+        })
     })
+    select('.chat-placeholder').addEventListener('click', function(){
+        let el1 = select('.text')
+        let el2 = select('input[name="search"]')
+        el1.style.display = el1.style.display === 'block' ? 'none' : 'block'
+        el2.style.display = el2.style.display === 'none' ? 'block' : 'none'
+    })
+    select('.chat-item').addEventListener('click')
 }
